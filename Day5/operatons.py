59d1e6e633662e4cee0863401566e2965e2024a2
kowshik8305 <kowshik8305@gmail.com>
2026-08-30 17:50:50 +0530

    string operations

---
+++ Day5/operatons.py
@@ -0,0 +1,87 @@
+n = input()
+n
+n
+'n'
+name = input()
+
+name
+'sai'
+age = int(input())
+21
+age
+21
+
+a = input()
+21
+a
+'21'
+n = input ("Enter the name:")
+n
+' sai benarji'
+n.split
+n.split()
+['sai', 'benarji']
+n = input('give name').split()
+n
+['sai', 'benarji']
+s = input('enter the vlaues').split()
+s
+['1', '2', '3', '3', '3', '4', '4', '5', '5', '5', '6', '6']
+map(int,s)
+map(int ,s)
+list(map(int,s))
+[1, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6]
+v = list(map(int,input().split()))
+v
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+v = list(map(float,input().split()))
+v
+[12.0, 22.3, 12.0, 22.0]
+d = tuple(input('enter the vlaues').split())
+d
+('1', '2', '3', '44', '55', '7')
+map(int,d)
+
+      
+(1, 2, 3, 44, 55, 7)
+v = tuple(map(int,input().split()))
+      
+
+v
+      
+(1, 2, 34, 4)
+b = list(map(float,input().split()))
+      
+
+b
+      
+[1.0, 2.3, 3.0, 4.0, 4454.0]
+a,b = [1,2]
+      
+a
+      
+1
+b
+      
+2
+a,b(1,2)
+      
+      
+a
+      
+1
+b
+      
+2
+email, password= input('Enter the email and password:').split()
+      
+'sai@gmail.com'
+password
+      
+'22222'
+
+a,b,c= list(map(int,input().split()))
+...      
+e = eval(input())
+...   
+... 
